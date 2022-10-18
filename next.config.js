3cd1d9b6f6aa1f4a7fda4b125266c964f9033ff1
winstonmeq <winstonmequila@gmail.com>
2022-10-18 15:54:44 +0800

    Update

--- next.config.js
+++ next.config.js
@@ -10,8 +10,8 @@ const nextConfig = {
     //dre eh butang ang domain.. dire lng mag change automatic na ma update ang mga domain
 
    
-     //NEXTAUTH_URL: 'https://deltaph.vercel.app',
-     NEXTAUTH_URL: 'http://192.168.102.18:3000',
+     NEXTAUTH_URL: 'https://cotrace.vercel.app',
+    // NEXTAUTH_URL: 'http://192.168.102.18:3000',
      NEXTAUTH_SECRET: 'v7COYqKpEdnCbd5aISAw9BxjupOLKYCgBVZ2kwusMNs=',
 
  },
