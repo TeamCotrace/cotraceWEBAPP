0bfa9c328ba89f9b29bc2340eb573dc627e29761
winstonmeq <winstonmequila@gmail.com>
2022-10-18 17:20:16 +0800

    update hotcars

--- pages/components/Hotcars/addhotcars.js
+++ pages/components/Hotcars/addhotcars.js
@@ -58,69 +58,7 @@ const Add_employee = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
 
   return (
-    
- <Flex>
- {/* {console.log(session.user._id)} */}
- <Button onClick={onOpen}>Add Employee</Button>
-
-<Modal isOpen={isOpen} onClose={onClose}>
-  
-  <ModalContent>
-  <Box px={8}>
-    <form onSubmit={signUpHandler}>        
-
-        <Box px={2} py={2} fontSize={26} >Add Plate Number</Box>
-
-
-       <Stack mt={5}>
-         
-       <InputGroup>
-       <InputLeftAddon>Plate Number</InputLeftAddon>
-
-         <Input
-           type="text"
-           value={pnumber}
-           onChange={(e) => {setpnumber(e.target.value)}}
-         />
-        
-       </InputGroup>
-
-       <InputGroup>
-       <InputLeftAddon>Vehicle Type</InputLeftAddon>
-
-         <Input
-           type="text"
-           value={typeV}
-           onChange={(e) => {settypeV(e.target.value)}}
-         />
-        
-       </InputGroup>
-       
-      
-       </Stack>
-   
-    
-       <ModalFooter>
-       <Button type="submit">
-        Save
-       </Button>
-       <Box w={5}></Box>
-       <Button colorScheme='blue' mr={3} onClick={onClose}>
-              Close
-       </Button>
-       </ModalFooter>
-
-
-     
-     </form>
-    </Box>
-
-
- 
-  </ModalContent>
-</Modal>
- </Flex>
-    
+ <>sdf</>
   );
 };
 export default Add_employee;
